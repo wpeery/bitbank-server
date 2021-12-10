674cd7d7795bf8c3e2ace919784260c940c5df08
Wesley Peery <peery@umail.ucsb.edu>
2021-12-09 19:56:00 -0800

    Initial commit with code from cookiecutter

---
+++ bitbank/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bitbank.users.models import User
+from bitbank.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
