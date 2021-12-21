f5e263cfbc4cc4280c035941015c008fe54f9967
wpeery <peery@umail.ucsb.edu>
2021-12-21 14:57:23 -0800

    Add satoshi column to users_user
    Also make changes to user model so we import modules not classes
    and methods.
    
    To make a migration I did the following:
    
    - Made changes to the user model
    - ran 'python manage.py makemigrations'
    - ran 'python manage.py migrate'
    - Made sure tests passed
    - Added the generated migration file to git

--- bitbank/users/models.py
+++ bitbank/users/models.py
@@ -1,16 +1,21 @@
-from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import models as auth_models
+from django.db import models as db_models
+from django import urls
+from django.utils import translation
 
 
-class User(AbstractUser):
+class User(auth_models.AbstractUser):
     """Default user for bitbank-server."""
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = db_models.CharField(
+        translation.gettext_lazy("Name of User"), blank=True, max_length=255
+    )
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    satoshis = db_models.PositiveBigIntegerField(
+        translation.gettext_lazy("Amount of satoshis owned"), default=0
+    )
 
     def get_absolute_url(self):
         """Get url for user's detail view.
@@ -19,4 +24,4 @@ class User(AbstractUser):
             str: URL for user detail.
 
         """
-        return reverse("users:detail", kwargs={"username": self.username})
+        return urls.reverse("users:detail", kwargs={"username": self.username})
